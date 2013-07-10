0fa62d4f04ff2c1842019478f6567c2f70406751
zhangzhe <zhzhfriend@163.com>
2013-07-10 21:14:19 +0800

    release
    release

---
+++ EP/Scripts/addClothesSize.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('#addClothesSize').click(function () {
+        var html = '<tr><td>';
+        html = html + '<input type="text" name="name" class="input-mini"/>';
+        html = html + '</td><td><input type="text" name="size" class="input-mini"/><button class="close tdCloseIcon">&times;</button></td></tr>';
+        $(html).appendTo($('#clothSizeTable'));
+        bindTdCloseIcon();
+    });
+});
+
+function bindTdCloseIcon() {
+    $('.tdCloseIcon').click(function () {
+        $(this).parentsUntil('tbody').remove();
+    });
+    $('input[name="name"] :last').autocomplete({
+        source: ['后中长', '胸围', '腰围', '臀围', '摆围', '内长'],
+        minLength: 0
+    }).autocomplete('search', '');
+}
